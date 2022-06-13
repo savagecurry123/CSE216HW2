f98d6a21b7b13ae6eb7df870865aac14508e7d22
Lokesh V <lokesh.vvk@gmail.com>
2022-06-12 23:38:11 -0400

    Test-Cases

--- Part B/js/Test.js
+++ Part B/js/Test.js
@@ -6,7 +6,9 @@ const KEY_LENGTH = 8;
 
 function printBST(header, tree) {
     let text = tree.toString() + "\n";
-    console.log(header + "\n" + text);
+    //console.log(header + "\n" + text);
+    text = header + "\n" + text;
+    console.log(text);
     let outputDisplay = document.getElementById("output-display");
     text = text.replaceAll(/(?:\r\n|\r|\n)/g, '<br>');
     text = text.replaceAll(" ", '&nbsp;');
@@ -19,6 +21,32 @@ function addPersonToBST(person, tree) {
 }
 
 let tree = new BinarySearchTree(KEY_LENGTH);
+let tree2 = new BinarySearchTree(KEY_LENGTH);
+let tree3 = new BinarySearchTree(KEY_LENGTH);
+
+tree3.putValue("1YUIOPLK", new Student("1YUIOPLK", "Rup", "Stone", 4.0));
+tree3.putValue("2QWEASDF", new Employee("2QWEASDF", "Mum", "Bum", 123123));
+tree3.putValue("3POIUYTRE", new Student("3POIUYTRE", "Hello", "Bye", 1.0));
+printBST("\nCurrent Binary Search Tree2: ", tree3);
+
+tree3.removeValue("1YUIOPLK");
+printBST("\nRemove Root", tree3);
+
+let aKey = tree2.generateKey();
+tree2.putValue(aKey, new Student(aKey, "Dan", "Chen", 3.2));
+let bKey = tree2.generateKey();
+tree2.putValue(bKey, new Employee(bKey, "Oliver", "M", 200));
+let cKey = tree2.generateKey();
+tree2.putValue(cKey, new Student(cKey, "Loki", "Thor", 4.0));
+let dKey = tree2.generateKey();
+tree2.putValue(dKey, new Employee(dKey, "Phoebe", "C", 100000));
+printBST("\nCurrent Binary Search Tree2: ", tree2);
+
+tree2.removeValue(aKey);
+printBST("\nRemove Root", tree2);
+
+tree2.removeValue(bKey);
+printBST("\nTest 2", tree2);
 
 // DEMONSTRATE ADDING VALUES TO THE BST, WHICH INCLUDES THE NEED TO MAKE THE BST BIGGER
 addPersonToBST(new Student(tree.generateKey(), "George", "Harrison", 4.0), tree);
@@ -50,14 +78,29 @@ console.log("\nget " + dgKey + ": " + p.toString() + "\n");
 tree.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
 tree.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
 tree.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
-printBST("\nAfter Changing 3 Items", tree);
+printBST("\nAfter Changing 3 Items:", tree);
 
 // AND DEMONSTRATE REMOVING ITEMS FROM THE BST
 tree.removeValue(jlKey);
-printBST("\nAfter Removing Otis Redding", tree);
+printBST("\nAfter Removing Otis Redding:", tree);
 
 tree.removeValue(cwKey);
-printBST("\nAfter Removing Keith Richards", tree);
+printBST("\nAfter Removing Keith Richards:", tree);
 
 tree.removeValue(dgKey);
-printBST("\nAfter Removing Bill Withers", tree);
+printBST("\nAfter Removing Bill Withers:", tree);
+
+let testKey1 = tree.generateKey();
+tree.putValue(testKey1, new Student(testKey1, "Bob", "Dom", 3.4));
+printBST("\nAfter Adding Bob Dom: ", tree);
+
+let testKey2 = tree.generateKey();
+tree.putValue(testKey2, new Employee(testKey2, "Steph", "Curry", 100));
+printBST("\nAfter Adding Steph Curry: ", tree);
+
+let testKey3 = tree.generateKey();
+tree.putValue(testKey3, new Student(testKey3, "J", "P", 4.0));
+printBST("\nAfter Adding JP: ", tree);
+
+tree.removeValue(testKey1);
+printBST("\nAfter Removing Bob Dom: ", tree);
